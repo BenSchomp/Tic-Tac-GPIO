8ceede95a1e0ca3c31004440fc19a0296fc34b0a
Ben Schomp <ben@benschomp.com>
2013-06-27 09:34:39 -0400

    some new constants, wrap the drivers in a function

--- animations.py
+++ animations.py
@@ -5,116 +5,127 @@ OUTPUT = 1
 OFF = 0
 ON = 1
 
+RED = 0
+GREEN = 1
+LED_OFFSET = [ 0, 9 ]
+RED_LEDS = range( 0, 9 )
+GREEN_LEDS = range( 9, 18 )
+
 def init():
   wiringpi.wiringPiSetup()
 
-  for pin in range( 0, 9 ):
+  for pin in RED_LEDS:
     wiringpi.pinMode( pin, OUTPUT )
 
-def ledOn( pin, delay=0 ):
-  ledsOn( [pin], delay )
+def ledOn( pin, duration=0 ):
+  ledsOn( [pin], duration )
 
-def ledOff( pin, delay=0 ):
-  ledsOff( [pin], delay )
+def ledOff( pin, duration=0 ):
+  ledsOff( [pin], duration )
 
-def ledOnOff( pin, delay=1000 ):
-  ledsOnOff( [pin], delay )
+def ledOnOff( pin, duration=1000 ):
+  ledsOnOff( [pin], duration )
 
-def ledsOn( pins, delay=0 ):
+def ledsOn( pins, duration=0 ):
   for pin in pins:
     wiringpi.digitalWrite( pin, ON )
-  wiringpi.delay( delay )
+  wiringpi.delay( duration )
 
-def ledsOff( pins, delay=0 ):
+def ledsOff( pins, duration=0 ):
   for pin in pins:
     wiringpi.digitalWrite( pin, OFF )
-  wiringpi.delay( delay )
+  wiringpi.delay( duration )
 
-def ledsOnOff( pins, delay=1000 ):
-  ledsOn( pins, delay )
-  ledsOff( pins, delay )
+def ledsOnOff( pins, duration=1000 ):
+  ledsOn( pins, duration )
+  ledsOff( pins, duration )
 
 
 
 ###
 
-def flashEach( delay ):
-  for pin in range(0,9):
-    ledOnOff( pin, delay )
+def flashOne( pin, duration ):
+  ledOnOff( pin, duration )
 
-def flashAll( delay ):
-  ledsOnOff( range(0, 9 ), delay )
+def flashAll( duration ):
+  ledsOnOff( RED_LEDS, duration )
 
-def flashRows( delay ):
+def flashEach( duration ):
+  for pin in RED_LEDS:
+    flashOne( pin, duration )
+
+def flashRows( duration ):
   for row in range( 0, 3 ):
-    ledsOnOff( [row*3 + 0, row*3 + 1, row*3 + 2 ], delay )
+    ledsOnOff( [row*3 + 0, row*3 + 1, row*3 + 2 ], duration )
 
-def flashCols( delay ):
+def flashCols( duration ):
   for col in range( 0, 3 ):
-    ledsOnOff( [col + 0, col + 3, col + 6 ], delay )
+    ledsOnOff( [col + 0, col + 3, col + 6 ], duration )
 
-def snake( delay ):
+def snake( duration ):
   trail = [0, 1, 2, 5, 4, 3, 6, 7, 8]
   for pin in trail:
-    ledOn( pin, delay )
+    ledOn( pin, duration )
 
   for pin in trail:
-    ledOff( pin, delay )
+    ledOff( pin, duration )
 
-def spiral( delay ):
+def spiral( duration ):
   trail = [0, 3, 6, 7, 8, 5, 2, 4, 1 ]
   for pin in trail:
-    ledOn( pin, delay )
+    ledOn( pin, duration )
 
-  wiringpi.delay( delay )
+  wiringpi.delay( duration )
 
   trail.reverse()
   for pin in trail:
-    ledOff( pin, delay )
+    ledOff( pin, duration )
 
-def flatWipe( delay ):
+def flatWipe( duration ):
   rows = [ [0,1,2], [3,4,5], [6,7,8] ]
   cols = [ [0,3,6], [1,4,7], [2,5,8] ]
 
   for i in range( 0, 2 ):
     for col in cols:
-      ledsOn( col, delay )
+      ledsOn( col, duration )
 
     for row in rows:
-      ledsOff( row, delay )
+      ledsOff( row, duration )
 
     cols.reverse()
     rows.reverse()
 
-def diagWipe( delay ):
+def diagWipe( duration ):
   diag1 = [ [0], [3,1], [6,4,2], [7,5], [8] ]
   diag2 = [ [2], [1,5], [0,4,8], [3,7], [6] ]
 
   for i in range( 0, 2 ):
     for diag in diag1:
-      ledsOn( diag, delay )
+      ledsOn( diag, duration )
 
     for diag in diag2:
-      ledsOff( diag, delay )
+      ledsOff( diag, duration )
 
     diag1.reverse()
     diag2.reverse()
 
 #####
 
-init()
+def go():
+  init()
 
-while 1:
-  flashEach( 125 )
-  flashRows( 150 )
-  flashCols( 150 )
-  snake( 100 )
-  flatWipe( 200 )
-  for i in range( 0, 2 ):
+  while 1:
+    flashEach( 125 )
+    flashRows( 150 )
+    flashCols( 150 )
+    snake( 100 )
+    flatWipe( 200 )
     spiral( 75 )
-  diagWipe( 150 )
-  for i in range( 0, 4 ):
-    flashAll( 125 )
-  wiringpi.delay( 500 )
+    diagWipe( 150 )
+
+    for i in range( 0, 3 ):
+      flashAll( 125 )
+
+    wiringpi.delay( 750 )
 
 
